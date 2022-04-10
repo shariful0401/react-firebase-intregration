dd860a977ed66c44da207befefe4e3e2c1319f5a
sharifulislam <shariful0401@gmail.com>
2022-04-10 22:59:22 +0600

    commit added

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+  const {singInWithGoogle} = useFirebase();
   return (
     <div>
       <h3>Please Login </h3>
+      <div style={{margin:'20px'}}>
+        <button onClick={singInWithGoogle}>Google Sign In</button>
+      </div>
+      <form>
+        <input type="email" placeholder='Your Email' />
+        <br />
+        <input type="password" placeholder='Your Password' />
+        <br />
+        <input type="submit" value="Login" />
+      </form>
     </div>
   );
 };
